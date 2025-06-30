c661d10e238d905f53d43cad80e6b81b6a1d0722
Seojun <auddml321@hanyang.ac.kr>
2025-06-30 15:05:23 +0900

    init project

---
+++ src/main/java/com/yoen/yoen_back/YoenBackApplication.java
@@ -0,0 +1,13 @@
+package com.yoen.yoen_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoenBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoenBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoen/yoen_back/YoenBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoen.yoen_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoenBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
