5a9a0b74d891b73f9a54782563a9810cc8e09890
Seojun <auddml321@hanyang.ac.kr>
2025-07-01 14:52:55 +0900

    feat: PrePaymentImage 작성

---
+++ src/main/java/com/yoen/yoen_back/entity/PrePaymentImage.java
@@ -0,0 +1,27 @@
+package com.yoen.yoen_back.entity;
+
+import com.yoen.yoen_back.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "prepaymentimages")
+public class PrePaymentImage extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long prePaymentImageId;
+
+    @JoinColumn(name = "prepayment_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private PrePayment prepayment;
+
+    @JoinColumn(name = "image_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Image image;
+}
