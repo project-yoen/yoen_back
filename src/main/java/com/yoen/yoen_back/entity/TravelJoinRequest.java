4d6f403eebd7bf516c65e4776825e930684c750b
leokang123 <leokang123@github.com>
2025-07-01 13:34:52 +0900

    Feat: TravelJoinRequest 엔티티 추가

---
+++ src/main/java/com/yoen/yoen_back/entity/TravelJoinRequest.java
@@ -0,0 +1,33 @@
+package com.yoen.yoen_back.entity;
+
+
+import com.yoen.yoen_back.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/** 여행 참여 신청 엔티티
+ * 여행 가입 신청을 한 유저들을 관리하는 테이블
+ */
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "traveljoinrequests")
+public class TravelJoinRequest extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long travelJoinRequestId;
+
+    @JoinColumn(name = "travel_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Travel travel;
+
+    @JoinColumn(name = "user_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    private Boolean isAccepted;
+}
