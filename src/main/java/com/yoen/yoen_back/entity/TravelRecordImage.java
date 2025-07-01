1fbfe25418fa536ab23101fb9e0112121e7a537c
Seojun <auddml321@hanyang.ac.kr>
2025-07-01 14:53:11 +0900

    feat: TravelRecordImage 작성

---
+++ src/main/java/com/yoen/yoen_back/entity/TravelRecordImage.java
@@ -0,0 +1,27 @@
+package com.yoen.yoen_back.entity;
+
+import com.yoen.yoen_back.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "travelrecordimages")
+public class TravelRecordImage extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long travelRecordImageId;
+
+    @JoinColumn(name = "travelrecord_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private TravelRecord travelrecord;
+
+    @JoinColumn(name = "image_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Image image;
+}
