795853b3693eb0f9073b9bf278748a54381f1099
Seojun <auddml321@hanyang.ac.kr>
2025-07-01 14:14:33 +0900

    feat: PaymentEntity 작성

---
+++ src/main/java/com/yoen/yoen_back/entity/Payment.java
@@ -0,0 +1,38 @@
+package com.yoen.yoen_back.entity;
+
+import com.yoen.yoen_back.common.entity.BaseEntity;
+import com.yoen.yoen_back.enums.Payer;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "payments")
+public class Payment extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+
+    @JoinColumn(name = "travel_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Travel travel;
+
+    @JoinColumn(name = "category_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Category category;
+
+    @Enumerated(EnumType.STRING)
+    private Payer payerType;
+
+    private LocalDateTime payTime;
+
+    private Long paymentAccount;
+
+}
