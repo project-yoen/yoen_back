fbbc7c48904d7bd8ee59ac7a061783dadbfdedf8
Seojun <auddml321@hanyang.ac.kr>
2025-07-02 11:29:57 +0900

    feat: 여행기록, 금액기록 가져오는데 필요한 JPA함수 작성

--- src/main/java/com/yoen/yoen_back/repository/payment/PaymentRepository.java
+++ src/main/java/com/yoen/yoen_back/repository/payment/PaymentRepository.java
@@ -3,5 +3,8 @@ package com.yoen.yoen_back.repository.payment;
 import com.yoen.yoen_back.entity.payment.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findByTravel_TravelId(Long travelId);
 }
--- src/main/java/com/yoen/yoen_back/repository/travel/TravelRecordRepository.java
+++ src/main/java/com/yoen/yoen_back/repository/travel/TravelRecordRepository.java
@@ -3,5 +3,8 @@ package com.yoen.yoen_back.repository.travel;
 import com.yoen.yoen_back.entity.travel.TravelRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TravelRecordRepository extends JpaRepository<TravelRecord, Long> {
+    List<TravelRecord> findByTravel_TravelId(Long travelTravelId);
 }
