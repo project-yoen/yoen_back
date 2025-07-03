3d321d7517b631cdf41307ffdd0c9c40230b70fc
Seojun <auddml321@hanyang.ac.kr>
2025-07-03 10:56:28 +0900

    Refactor: DTO 이름 변경

---
+++ src/main/java/com/yoen/yoen_back/dto/PaymentRequestDto.java
@@ -0,0 +1,9 @@
+package com.yoen.yoen_back.dto;
+
+import com.yoen.yoen_back.entity.Category;
+import com.yoen.yoen_back.enums.Payer;
+
+public record PaymentRequestDto(Long paymentId, Long travelId, Category category, Payer payerType, String payTime,
+                                Long paymentAccount) {
+}
+
