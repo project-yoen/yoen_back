bff5130874a2eef6ff2ad9904fb60eccbc4ad86f
leokang123 <leokang123@github.com>
2025-07-03 13:27:12 +0900

    feat: GlobalExceptionHandler 로그 적용 방식 변경

--- src/main/java/com/yoen/yoen_back/handler/GlobalExceptionHandler.java
+++ src/main/java/com/yoen/yoen_back/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.yoen.yoen_back.handler;
 
 import com.yoen.yoen_back.common.entity.ApiException;
 import com.yoen.yoen_back.dto.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  *  컨트롤러에서 트라이 캐치를 전부 없애고 서비스에서만 트라이캐치하기 위해
  *  따로 에러 핸들러를 추가함
  */
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -31,7 +33,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<?>> handleOther(Exception ex) {
         // 로깅 추가 가능
-        ex.printStackTrace();
+        log.error("Unhandled exception caught", ex);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ApiResponse.failure("서버 내부 오류가 발생했습니다."));
