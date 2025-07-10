1c20ac81a4c0dcaba88132b37c0e5270d7258164
Seojun <auddml321@hanyang.ac.kr>
2025-07-10 18:21:00 +0900

    feat: 잡다한 Dto 생성

---
+++ src/main/java/com/yoen/yoen_back/dto/TravelDestinationDto.java
@@ -0,0 +1,4 @@
+package com.yoen.yoen_back.dto;
+
+public record TravelDestinationDto(Long travelId, String destination) {
+}
---
+++ src/main/java/com/yoen/yoen_back/dto/TravelUserDto.java
@@ -0,0 +1,4 @@
+package com.yoen.yoen_back.dto;
+
+public record TravelUserDto(Long travelId, Long userId) {
+}
