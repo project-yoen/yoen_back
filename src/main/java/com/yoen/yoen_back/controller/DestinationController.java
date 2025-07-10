9437564c245745fca99e594814cea21203e34ad5
Seojun <auddml321@hanyang.ac.kr>
2025-07-10 18:19:58 +0900

    feat: 목적지 테스트를 위한 Dto와 컨트롤러 작성

---
+++ src/main/java/com/yoen/yoen_back/controller/DestinationController.java
@@ -0,0 +1,24 @@
+package com.yoen.yoen_back.controller;
+
+import com.yoen.yoen_back.dto.DestinationDto;
+import com.yoen.yoen_back.entity.travel.Destination;
+import com.yoen.yoen_back.service.TravelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/destination")
+public class DestinationController {
+    private final TravelService travelService;
+
+    @PostMapping("/create")
+    public List<Destination> createDestination(@RequestBody List<DestinationDto> destinations) {
+        return travelService.createDestinations(destinations);
+    }
+}
---
+++ src/main/java/com/yoen/yoen_back/dto/DestinationDto.java
@@ -0,0 +1,6 @@
+package com.yoen.yoen_back.dto;
+
+import com.yoen.yoen_back.enums.Nation;
+
+public record DestinationDto(String name, Nation nation) {
+}
