1e495c0d36f829d833074e9ceb5f5b68284423d9
leokang123 <leokang123@github.com>
2025-07-16 16:39:58 +0900

    feat: TravelUserRepository에 travelId와 user로 travelUserId찾기

--- src/main/java/com/yoen/yoen_back/repository/travel/TravelUserRepository.java
+++ src/main/java/com/yoen/yoen_back/repository/travel/TravelUserRepository.java
@@ -1,10 +1,14 @@
 package com.yoen.yoen_back.repository.travel;
 
 import com.yoen.yoen_back.entity.travel.TravelUser;
+import com.yoen.yoen_back.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TravelUserRepository extends JpaRepository<TravelUser, Long> {
     List<TravelUser> findByTravel_TravelId(Long travelId);
+    Optional<TravelUser> findByTravel_TravelIdAndUser(Long travelId, User user);
+
 }
