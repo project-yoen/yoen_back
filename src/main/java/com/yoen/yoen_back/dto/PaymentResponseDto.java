8fe92f74b1f9ac98b5a71c477fc1dc2abd80af4d
Seojun <auddml321@hanyang.ac.kr>
2025-07-17 15:31:35 +0900

    feat: ResponseDto의 시간 형식을 LocalDateTime으로 변경

--- src/main/java/com/yoen/yoen_back/dto/PaymentResponseDto.java
+++ src/main/java/com/yoen/yoen_back/dto/PaymentResponseDto.java
@@ -3,7 +3,8 @@ package com.yoen.yoen_back.dto;
 import com.yoen.yoen_back.entity.Category;
 import com.yoen.yoen_back.enums.Payer;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-public record PaymentResponseDto (Long paymentId, Category category, Payer payerType, String payTime, Long paymentAccount, List<PaymentImageDto> images){
+public record PaymentResponseDto (Long paymentId, Category category, Payer payerType, LocalDateTime payTime, Long paymentAccount, List<PaymentImageDto> images){
 }
