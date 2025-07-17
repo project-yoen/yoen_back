675903526cf6ffb456c1cc301da3f0b5f1089427
Seojun <auddml321@hanyang.ac.kr>
2025-07-17 15:50:40 +0900

    feat: DTO에서 카테고리 객체를 받지 않고 ID를 받도록 변경

--- src/main/java/com/yoen/yoen_back/dto/PaymentRequestDto.java
+++ src/main/java/com/yoen/yoen_back/dto/PaymentRequestDto.java
@@ -3,7 +3,7 @@ package com.yoen.yoen_back.dto;
 import com.yoen.yoen_back.entity.Category;
 import com.yoen.yoen_back.enums.Payer;
 
-public record PaymentRequestDto(Long paymentId, Long travelId, Category category, Payer payerType, String payTime,
+public record PaymentRequestDto(Long paymentId, Long travelId, Long categoryId, Payer payerType, String payTime,
                                 Long paymentAccount) {
 }
 
