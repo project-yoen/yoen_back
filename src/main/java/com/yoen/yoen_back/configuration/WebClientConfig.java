bcdfcbb2e8fad3c3c09743b4528fd6de36f89f72
leokang123 <leokang123@github.com>
2025-07-22 14:35:07 +0900

    feat: WebClient 사용하여 환율 api 요청 기능구현

---
+++ src/main/java/com/yoen/yoen_back/configuration/WebClientConfig.java
@@ -0,0 +1,19 @@
+package com.yoen.yoen_back.configuration;
+
+import org.apache.http.HttpHeaders;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webCLient(WebClient.Builder builder) {
+        return builder
+                .baseUrl("https://m.search.naver.com/p/csearch/content/qapirender.nhn?key=calculator&pkid=141&q=%ED%99%98%EC%9C%A8&where=m&u1=keb&u6=standardUnit&u7=0&u3=JPY&u4=KRW&u8=down&u2=1")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+}
