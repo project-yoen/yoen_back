cdf6732fea894a67f30df299e1ca94d5ed673fae
leokang123 <leokang123@github.com>
2025-07-23 11:11:01 +0900

    feat: TravelJoinRequest 중 isActive isAccept로 쿼리 함수

--- src/main/java/com/yoen/yoen_back/repository/travel/TravelJoinRequestRepository.java
+++ src/main/java/com/yoen/yoen_back/repository/travel/TravelJoinRequestRepository.java
@@ -1,7 +1,11 @@
 package com.yoen.yoen_back.repository.travel;
 
 import com.yoen.yoen_back.entity.travel.TravelJoinRequest;
+import com.yoen.yoen_back.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TravelJoinRequestRepository extends JpaRepository<TravelJoinRequest, Long> {
+    List<TravelJoinRequest> findByUserAndIsActiveTrueAndIsAcceptedFalse(User user);
 }
