24ff4eff27bded2d46443b0b851b6b9ae15e9e61
Seojun <auddml321@hanyang.ac.kr>
2025-07-23 16:33:23 +0900

    refactor: DestinationController 리팩토링
    - CommonController로 이관

--- src/main/java/com/yoen/yoen_back/controller/CommonController.java
+++ src/main/java/com/yoen/yoen_back/controller/CommonController.java
@@ -2,7 +2,9 @@ package com.yoen.yoen_back.controller;
 
 import com.yoen.yoen_back.common.entity.ApiResponse;
 import com.yoen.yoen_back.dto.etc.CategoryRequestDto;
+import com.yoen.yoen_back.dto.etc.DestinationDto;
 import com.yoen.yoen_back.dto.travel.TravelDestinationDto;
+import com.yoen.yoen_back.entity.travel.Destination;
 import com.yoen.yoen_back.service.CommonService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +18,14 @@ import java.util.List;
 public class CommonController {
     private final CommonService commonService;
 
+    /** 카테고리 관련 엔드포인트 **/
     @PostMapping("/category/create")
     public ResponseEntity<ApiResponse<CategoryRequestDto>> createCategory(@RequestBody CategoryRequestDto dto) {
         CategoryRequestDto category = commonService.createCategory(dto);
         return ResponseEntity.ok(ApiResponse.success(category));
     }
 
+    /** 목적지 관련 엔드포인트 **/
     @GetMapping("/destination/all")
     public List<TravelDestinationDto> getAllTravelDestinations() {
         return commonService.getAllTravelDestination().stream().map(
@@ -29,5 +33,8 @@ public class CommonController {
         ).toList();
     }
 
-
+    @PostMapping("/destination/create")
+    public List<Destination> createDestination(@RequestBody List<DestinationDto> destinations) {
+        return commonService.createDestinations(destinations);
+    }
 }
