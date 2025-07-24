a01646cf0eb34ff61a7ac0ad0f5109ea46a4ec06
leokang123 <leokang123@github.com>
2025-07-24 17:05:29 +0900

    refactor: 필요없는 필드 dto에서 삭제

--- src/main/java/com/yoen/yoen_back/dto/travel/TravelRecordImageDto.java
+++ src/main/java/com/yoen/yoen_back/dto/travel/TravelRecordImageDto.java
@@ -1,4 +1,4 @@
 package com.yoen.yoen_back.dto.travel;
 
-public record TravelRecordImageDto(Long travelRecordImageId, Long imageId, String imageUrl) {
+public record TravelRecordImageDto(Long travelRecordImageId, String imageUrl) {
 }
