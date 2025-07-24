e8aa2763844814317e1edff4127d009376dd5738
Seojun <auddml321@hanyang.ac.kr>
2025-07-24 14:59:59 +0900

    feat: 테이블 구조가 변함에 따라 dto 변경

--- src/main/java/com/yoen/yoen_back/dto/payment/PaymentRequestDto.java
+++ src/main/java/com/yoen/yoen_back/dto/payment/PaymentRequestDto.java
@@ -3,10 +3,11 @@ package com.yoen.yoen_back.dto.payment;
 import com.yoen.yoen_back.dto.payment.settlement.SettlementRequestDto;
 import com.yoen.yoen_back.enums.Payer;
 import com.yoen.yoen_back.enums.PaymentMethod;
+import com.yoen.yoen_back.enums.PaymentType;
 
 import java.util.List;
 
 public record PaymentRequestDto(Long paymentId, Long travelId, Long travelUserId, Long categoryId, Payer payerType, String payTime,
                                 Long paymentAccount, String paymentName, PaymentMethod paymentMethod,
-                                List<SettlementRequestDto> settlementList) {
+                                List<SettlementRequestDto> settlementList, PaymentType paymentType) {
 }
--- src/main/java/com/yoen/yoen_back/dto/payment/PaymentResponseDto.java
+++ src/main/java/com/yoen/yoen_back/dto/payment/PaymentResponseDto.java
@@ -2,11 +2,12 @@ package com.yoen.yoen_back.dto.payment;
 
 import com.yoen.yoen_back.enums.Payer;
 import com.yoen.yoen_back.enums.PaymentMethod;
+import com.yoen.yoen_back.enums.PaymentType;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record PaymentResponseDto(Long paymentId, Long categoryId, String categoryName, Payer payerType,
-                                 PaymentMethod paymentMethod, String paymentName, Double exchangeRate,
+                                 PaymentMethod paymentMethod, String paymentName, PaymentType paymentType, Double exchangeRate,
                                  LocalDateTime payTime, Long paymentAccount, List<PaymentImageDto> images) {
 }
