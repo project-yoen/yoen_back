2d77789157df2ac73b61aa0baeeafcffe3c2c6f5
leokang123 <leokang123@github.com>
2025-07-25 16:13:19 +0900

    feat: travel 관련 권한 기능 구현

--- src/main/java/com/yoen/yoen_back/service/TravelService.java
+++ src/main/java/com/yoen/yoen_back/service/TravelService.java
@@ -2,6 +2,7 @@ package com.yoen.yoen_back.service;
 
 import com.yoen.yoen_back.common.utils.Formatter;
 import com.yoen.yoen_back.dto.travel.TravelRequestDto;
+import com.yoen.yoen_back.dto.travel.TravelUserDto;
 import com.yoen.yoen_back.entity.travel.Travel;
 import com.yoen.yoen_back.entity.travel.TravelUser;
 import com.yoen.yoen_back.entity.user.User;
@@ -28,17 +29,16 @@ public class TravelService {
     }
 
 
-
-
     //Todo 여행을 삭제할 때 관련된 모든 테이블의 레코드를 비활성화 해야 할까?
     @Transactional
-    public void deleteTravel(Long travelId) {
+    public void deleteTravel(Travel tv) {
 //        List<TravelRecord> tr = travelRecordRepository.findByTravel_TravelId(travelId);
 //        List<TravelUser> tu = travelUserRepository.findByTravel_TravelId(travelId);
 //        List<TravelDestination> td = travelDestinationRepository.findByTravel_TravelId(travelId);
 //        List<Payment> pay= paymentRepository.findByTravel_TravelId(travelId);
 //        List<PrePayment> prePayment = paymentRepository.findByTravel_TravelId(travelId);
-        travelRepository.deleteById(travelId);
+        tv.setIsActive(false);
+        travelRepository.save(tv);
     }
 
     // 여행 객체를 저장 -> 여행 객체와 유저를 매핑 -> 여행 객체에 여행_목적지 객체 매핑 -> 함수 3개를 모은 createTravel 선언
@@ -92,21 +92,18 @@ public class TravelService {
     }
 
 
-
-    public List<TravelUser> getAllTravelUser() {
-        return travelUserRepository.findAll();
+    public List<TravelUserDto> getAllTravelUser(Travel tv) {
+        List<TravelUser> tuList = travelUserRepository.findByTravelAndIsActiveTrue(tv);
+        return tuList.stream().map(tu -> new TravelUserDto(tu.getTravelUserId(), tu.getTravel().getTravelId(), tu.getUser().getUserId(), tu.getRole(), tu.getTravelNickname())).toList();
     }
-
 
 
     // 여행에 대한 여행 유저 반환하는 함수
-    public TravelUser getTravelUser(User user, Long travelId) {
-        return travelUserRepository.findByTravel_TravelIdAndUserAndIsActiveTrue(travelId, user)
+    public TravelUserDto getTravelUser(User user, Travel tv) {
+        TravelUser tu = travelUserRepository.findByTravelAndUserAndIsActiveTrue(tv, user)
                 .orElseThrow(() -> new RuntimeException("해당 유저의 TravelUser가 존재하지 않습니다."));
+        return new TravelUserDto(tu.getTravelUserId(), tu.getUser().getUserId(), tu.getTravel().getTravelId(), tu.getRole(), tu.getTravelNickname());
     }
-
-
-
 
 
 }
