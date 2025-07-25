b040105e57f5d21f08830871d5cc3ac6bfc4bc8c
Seojun <auddml321@hanyang.ac.kr>
2025-07-25 13:08:47 +0900

    feat: 금액기록 세부내용 확인 메서드 작성
    - 금액기록을 선택했을 때 세부적인 내용을 반환할 getDetailPayment 함수 작성

--- src/main/java/com/yoen/yoen_back/repository/image/PaymentImageRepository.java
+++ src/main/java/com/yoen/yoen_back/repository/image/PaymentImageRepository.java
@@ -1,6 +1,7 @@
 package com.yoen.yoen_back.repository.image;
 
 import com.yoen.yoen_back.entity.image.PaymentImage;
+import com.yoen.yoen_back.entity.payment.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.Optional;
 public interface PaymentImageRepository extends JpaRepository<PaymentImage, Long> {
     Optional<PaymentImage> findByPaymentImageIdAndIsActiveTrue(Long id);
     List<PaymentImage> findByPayment_PaymentId(Long id);
+
+    List<PaymentImage> findByPayment(Payment pm);
 }
