6b86b0da5e9d8f10bfb60fa8044ce853ff85c5b2
Seojun <auddml321@hanyang.ac.kr>
2025-07-30 14:17:47 +0900

    feat: 첫 여행기록의 이미지를 가져오는 JPA 메서드 선언

--- src/main/java/com/yoen/yoen_back/repository/image/TravelRecordImageRepository.java
+++ src/main/java/com/yoen/yoen_back/repository/image/TravelRecordImageRepository.java
@@ -1,8 +1,10 @@
 package com.yoen.yoen_back.repository.image;
 
+import com.yoen.yoen_back.entity.image.Image;
 import com.yoen.yoen_back.entity.image.TravelRecordImage;
 import com.yoen.yoen_back.entity.travel.TravelRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +15,7 @@ public interface TravelRecordImageRepository extends JpaRepository<TravelRecordI
     List<TravelRecordImage> findAllByTravelRecord_TravelRecordId(Long travelRecordId);
 
     List<TravelRecordImage> findByTravelRecordAndIsActiveTrue(TravelRecord tr);
+
+    @Query("SELECT tri.image FROM TravelRecordImage tri WHERE tri.travelRecord.travelRecordId = :travelRecordId AND tri.travelRecord.isActive = true")
+    Optional<Image> findFirstByTravelRecordOrderByCreatedAtAsc(Long travelRecordId);
 }
