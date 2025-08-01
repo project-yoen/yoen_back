6260483f96def2e7320b66cbd6d136285ee2dd00
Seojun <auddml321@hanyang.ac.kr>
2025-08-01 13:32:18 +0900

    feat: 여행에 속한 유저 수를 DTOd

--- src/main/java/com/yoen/yoen_back/dto/travel/TravelResponseDto.java
+++ src/main/java/com/yoen/yoen_back/dto/travel/TravelResponseDto.java
@@ -2,5 +2,5 @@ package com.yoen.yoen_back.dto.travel;
 
 import java.time.LocalDate;
 
-public record TravelResponseDto(Long travelId, String travelName, LocalDate startDate, LocalDate endDate,String imageUrl) {
+public record TravelResponseDto(Long travelId, Long numOfPeople, String travelName, LocalDate startDate, LocalDate endDate,String travelImageUrl) {
 }
