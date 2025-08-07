5846eb8d832bbb73aaedefd32869c5f8af99669d
Seojun <auddml321@hanyang.ac.kr>
2025-08-07 12:37:10 +0900

    chore: todo 완료한거 제거

--- src/main/java/com/yoen/yoen_back/dto/travel/TravelResponseDto.java
+++ src/main/java/com/yoen/yoen_back/dto/travel/TravelResponseDto.java
@@ -3,6 +3,5 @@ package com.yoen.yoen_back.dto.travel;
 import com.yoen.yoen_back.enums.Nation;
 
 import java.time.LocalDate;
-// Todo: 여기도 현재 인원수 추가하고 프론트에서 처리해야함
 public record TravelResponseDto(Long travelId, Long numOfPeople, Long numOfJoinedPeople, Nation nation, Long sharedFund, String travelName, LocalDate startDate, LocalDate endDate, String travelImageUrl) {
 }
