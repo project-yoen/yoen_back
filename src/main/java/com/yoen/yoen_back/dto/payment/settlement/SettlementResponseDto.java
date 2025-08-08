af7428da7ddf9850978ecf930c074398ea784def
leokang123 <leokang123@github.com>
2025-08-09 04:25:25 +0900

    chore: 불필요한 요소 삭제

--- src/main/java/com/yoen/yoen_back/dto/payment/settlement/SettlementResponseDto.java
+++ src/main/java/com/yoen/yoen_back/dto/payment/settlement/SettlementResponseDto.java
@@ -1,10 +1,9 @@
 package com.yoen.yoen_back.dto.payment.settlement;
 
-import com.yoen.yoen_back.dto.travel.TravelUserDto;
 import com.yoen.yoen_back.dto.travel.TravelUserResponseDto;
 
 import java.util.List;
 
 public record SettlementResponseDto(Long settlementId, Long paymentId, String settlementName, Long amount, Boolean isPaid,
-                                   List<TravelUserResponseDto> travelUsers) {
+                                    List<TravelUserResponseDto> travelUsers) {
 }
