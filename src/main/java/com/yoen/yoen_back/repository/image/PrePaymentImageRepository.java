11568c7fb121feae19ecf816d28f84fa1abc826d
Seojun <auddml321@hanyang.ac.kr>
2025-08-11 16:10:06 +0900

    rm: PrePayment 관련 파일들 삭제

--- src/main/java/com/yoen/yoen_back/repository/image/PrePaymentImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yoen.yoen_back.repository.image;
-
-import com.yoen.yoen_back.entity.image.PrePaymentImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrePaymentImageRepository extends JpaRepository<PrePaymentImage, Long> {
-}
--- src/main/java/com/yoen/yoen_back/repository/payment/PrePaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yoen.yoen_back.repository.payment;
-
-import com.yoen.yoen_back.entity.payment.PrePayment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrePaymentRepository extends JpaRepository<PrePayment, Long> {
-}
