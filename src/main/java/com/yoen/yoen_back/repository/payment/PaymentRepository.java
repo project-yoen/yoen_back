e532ecd8ddc00215239eb824133effe9dd35edf6
Seojun <auddml321@hanyang.ac.kr>
2025-08-11 17:23:45 +0900

    feat: PaymentType에 따라서 Payment를 찾아오기 위해 JPA메서드 추가

--- src/main/java/com/yoen/yoen_back/repository/payment/PaymentRepository.java
+++ src/main/java/com/yoen/yoen_back/repository/payment/PaymentRepository.java
@@ -2,18 +2,29 @@ package com.yoen.yoen_back.repository.payment;
 
 import com.yoen.yoen_back.entity.payment.Payment;
 import com.yoen.yoen_back.entity.travel.Travel;
+import com.yoen.yoen_back.enums.PaymentType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     List<Payment> findByTravel_TravelIdAndIsActiveTrue(Long travelId);
 
-    List<Payment> findAllByTravelAndPayTimeBetweenAndIsActiveTrue(Travel tv, LocalDateTime localDateTime, LocalDateTime localDateTime1);
+    List<Payment> findAllByTravelAndPaymentTypeAndPayTimeBetweenAndIsActiveTrue(Travel tv, LocalDateTime localDateTime, LocalDateTime localDateTime1, PaymentType paymentType);
 
     List<Payment> findByTravelAndIsActiveTrue(Travel travel);
 
     Optional<Payment> findByPaymentIdAndIsActiveTrue(Long paymentId);
+
+    List<Payment> findAllByTravelAndPaymentTypeAndIsActiveTrue(Travel tv, PaymentType paymentType);
+
+    List<Payment> findAllByTravelAndTypeInAndPayTimeBetweenAndIsActiveTrue(
+            Travel tv,
+            Collection<PaymentType> types,
+            LocalDateTime localDateTime,
+            LocalDateTime localDateTime1
+    );
 }
